80cbbbed143fe5d5cf8fbd007ccad5d81cb93cff
Michael Mänz <michael.maenz@iconten.de>
2021-04-04 21:18:32 +0200

    java project cleanup

---
+++ org.eclipse.swt.cef/java/src/main/java/org/eclipse/swt/cef/Chromium.java
@@ -0,0 +1,121 @@
+/*********************************************************************
+ * Copyright (c) 2019 QNX Software Systems and others
+ *
+ * This program and the accompanying materials are made
+ * available under the terms of the Eclipse Public License 2.0
+ * which is available at https://www.eclipse.org/legal/epl-2.0/
+ *
+ * SPDX-License-Identifier: EPL-2.0
+ *********************************************************************/
+package org.eclipse.swt.cef;
+
+import java.io.File;
+import java.io.IOException;
+import java.net.URISyntaxException;
+
+import org.eclipse.core.runtime.Path;
+import org.eclipse.swt.SWT;
+import org.eclipse.swt.graphics.Rectangle;
+import org.eclipse.swt.internal.DPIUtil;
+//import org.eclipse.swt.internal.gtk.GDK;
+//import org.eclipse.swt.internal.gtk.GTK;
+//import org.eclipse.swt.internal.gtk.GdkWindowAttr;
+import org.eclipse.swt.widgets.Canvas;
+import org.eclipse.swt.widgets.Composite;
+import org.eclipse.swt.widgets.Control;
+import org.eclipse.swt.widgets.Listener;
+
+public class Chromium extends Canvas {
+
+	static {
+		try {
+			File cefLibDir = Activator.getBundleLocation(new Path("cef/lib/libswt-cef.so"));
+			System.load(cefLibDir.getAbsolutePath());
+		} catch (URISyntaxException | IOException e) {
+			Activator.log("Error loading cef library", e);
+		}
+	}
+
+	private long chromiumWindow;
+	private long xWindow;
+
+	public Chromium(Composite parent, int style) {
+		super(parent, style);
+
+		long window = handle;
+		
+		/*
+		GTK.gtk_widget_realize(handle);
+		long window = GTK.gtk_widget_get_window(handle);
+
+		GdkWindowAttr attrs = new GdkWindowAttr();
+		attrs.width = 1;
+		attrs.height = 1;
+		attrs.event_mask = GDK.GDK_KEY_PRESS_MASK | GDK.GDK_KEY_RELEASE_MASK | GDK.GDK_FOCUS_CHANGE_MASK
+				| GDK.GDK_POINTER_MOTION_MASK | GDK.GDK_BUTTON_PRESS_MASK | GDK.GDK_BUTTON_RELEASE_MASK
+				| GDK.GDK_ENTER_NOTIFY_MASK | GDK.GDK_LEAVE_NOTIFY_MASK | GDK.GDK_EXPOSURE_MASK
+				| GDK.GDK_POINTER_MOTION_HINT_MASK;
+		attrs.window_type = GDK.GDK_WINDOW_CHILD;
+
+		*/
+		Listener listener = event -> {
+			switch (event.type) {
+			case SWT.Resize:
+				Rectangle clientArea = DPIUtil.autoScaleUp(getClientArea());
+				break;
+			case SWT.Dispose:
+				if (chromiumWindow != 0) {
+					dispose();
+					chromiumWindow = 0;
+				}
+				break;
+			}
+		};
+		addListener(SWT.Resize, listener);
+		addListener(SWT.Dispose, listener);
+
+		getDisplay().timerExec(1000, () -> {
+			start(xWindow);
+			Runnable loop = new Runnable() {
+				@Override
+				public void run() {
+					System.out.println("working");
+					work();
+					System.out.println("  done");
+					getDisplay().timerExec(20, this);
+				}
+			};
+			getDisplay().timerExec(20, loop);
+		});
+	}
+
+	public long getHandle() {
+		long handle = 0;
+		java.lang.reflect.Field _viewField;
+		java.lang.reflect.Field _idField;
+		try {
+			/*
+			if (Platform.isMac()) {
+				_viewField = Control.class.getDeclaredField("view");
+				final Object view = _viewField.get(this);
+				final Class<?> idClass = Class.forName("org.eclipse.swt.internal.cocoa.id");
+				_idField = idClass.getDeclaredField("id");
+				handle = _idField.getLong(view);
+			} else {*/
+				_idField = Control.class.getDeclaredField("handle");
+				handle = _idField.getLong(this);
+			//}
+		} catch (final Exception e) {
+			e.printStackTrace();
+		}
+		return handle;
+	}
+
+	native static int start(long parentId);
+
+	native static int work();
+
+	public void setUrl(String url) {
+	}
+
+}
